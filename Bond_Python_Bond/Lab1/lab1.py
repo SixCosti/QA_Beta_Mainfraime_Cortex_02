d921c2f7546a09e69bafa86ce8e66ca453b3bbaf
Costi <sixcosti@yahoo.com>
2023-08-07 14:05:18 +0100

    This is Morning Lesson.

---
+++ Bond_Python_Bond/Lab1/lab1.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+
+username = input("Please enter your codename: ")
+
+if username == "Bob" :
+    print("Admised!")
+else :
+    print("You're barking at the wrong door.")
+
+age = input("SecretCode: How many spring seasons? >>> ")
+
+if age == "32":
+    print(username," is " + str(age) + " years old.")
+else:
+    print(username," is " + str(age) + " years old.")
+
---
+++ Bond_Python_Bond/Lab1/w_l.py
@@ -0,0 +1,5 @@
+length = input("length: ")
+width = input("width: ")
+
+print("The rentagle diameter is : ", ((int(length) + int(width)) * 2))
+
