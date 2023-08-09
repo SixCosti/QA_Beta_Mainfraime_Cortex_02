dddc73a648d7c35c8d08b9c0ef5424a49df8fecd
Admin <sixcosti@yahoo.com>
2023-08-09 14:27:30 +0100

    Test

--- Bond_Python_Bond/Lab6/tax.py
+++ Bond_Python_Bond/Lab6/tax.py
@@ -1,8 +1,8 @@
 
 
 allowance = 11850
-poor_class = 34500
-poor_class_tax = 20/100
+working_class = 34500
+working_class_tax = 20/100
 middle_class = range(34501, 150000)
 middle_class_tax = 40/100
 rich = (150000 + 1)
@@ -12,25 +12,26 @@ rich_tax = 45/100
 
 
 def getIncomeTax(salary):
-    #if after_tax in salary:#
-    if True:
-        salary = int(input("Type your yearly income: "))
-        if salary in range(0, 34500):
-            after_tax == (salary - 11850)/20
-            print(after_tax)
-            return after_tax
-        elif salary in range(34501 - 150000):
-            after_tax = (salary - 11850)/40
-            return after_tax
-        elif salary >= 150000:
-            after_tax = (salary - 11850)/45
-            return after_tax
-        else:
-            return None
+    if salary <= working_class:
+
+        after_tax = (salary - allowance) * working_class_tax
+        return after_tax
+    elif salary in middle_class:
+        after_tax = (salary - allowance) * middle_class_tax
+        return after_tax
+    elif salary >= rich:
+        after_tax = (salary - allowance) * rich_tax
+        return after_tax
     else:
-        print("Nope")
+        return None
+   
 
+wage = int(input("Type your yearly income: "))
+income_tax = getIncomeTax(wage)
 
-
-wage = 150000
-getIncomeTax(wage)
+while True:
+    if income_tax is not None:
+        print("Here is your wage after tax: {}".format(income_tax))
+    else:
+        print("Wage out of range!")
+    break
