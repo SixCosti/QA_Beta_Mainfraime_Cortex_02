4591ce7a1566dad82e9480cbd4e37c0980aaa26d
Admin <sixcosti@yahoo.com>
2023-08-14 16:16:34 +0100

    OOP Classes

--- Bond_Python_Bond/Lab9/class_minilab.py
+++ Bond_Python_Bond/Lab9/class_minilab.py
@@ -1,39 +1,18 @@
-#Goal: “Create a Budget class that can instantiate objects based on different budget categories:
-# like food, clothing, and entertainment. 
-# These objects should allow for depositing and withdrawing funds from each category, 
-# as well computing category balances and transferring balance amounts between categories”
+class Bird:
+    #def _init_(self, feathersvar, colourvar):
+    #self.feathers = feathersvar
+    #self.colour = colourvar
+    feathers = 0
 
-#class Budget:
-#    pass
+    def feathers(self):
+        self.feathers += 1
 
-#clothing = Budget()
-#entertainment = Budget()
+class Owl(Bird):
+    def feathers(self):
+        self.feathers += 150
 
-#clothing.withdraw(20)
-#entertainment.deposit(5)
-
-class Budget:
-
-    #def __init__(self, foodvar, clothingvar, entertaimentvar)
-    def __init__(self, fundsvar):
-        self.funds = fundsvar
-        #self.food = foodvar
-        #self.clothing = clothingvar
-        #self.entertaiment = entertaimentvar
-        #return(self.food, self.clothing, self.entertaiment)
-
-    def withdraw(self, withdrawal):
-        self.withdrawing = withdrawal
-        after_withdrawal = self.funds - after_withdrawing
-        return after_withdrawal
-    
-    def deposit(self, depositing):
-        self.deposited = depositing
-        after_deposited = self.funds + deposited
-        return after_deposited
-
-clothing = Budget(200)
-food = Budget(6)
-entertaiment = Budget(50)
-
-print(clothing.withdraw(20))
+joe_the_owl = Bird()
+joe_the_owl.feathers()
+joe_the_owl.feathers()
+joe_the_owl.feathers()
+print(joe_the_owl.feathers)
